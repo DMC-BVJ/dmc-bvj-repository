e5e971739dba45e786da1ff899f5b75d4c33d381
krvavizmaj <krvavizmaj@gmail.com>
2013-04-11 15:00:17 +0200

    test commit

--- DMCtask2/src/mk/bvj/dmc/learner/Learner.java
+++ DMCtask2/src/mk/bvj/dmc/learner/Learner.java
@@ -3,6 +3,7 @@ package mk.bvj.dmc.learner;
 /**
  * @author Boris
  * Implementation of the algorithm that will train on the training data from task 1.
+ * Arsov commit test.
  */
 public class Learner {
 
