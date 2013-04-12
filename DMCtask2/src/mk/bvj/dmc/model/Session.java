524252e38337c1b8aef002cc9006502d062b0083
borissim <boris.simovski@gmail.com>
2013-04-12 19:43:13 +0200

    proba

--- DMCtask2/src/mk/bvj/dmc/model/Session.java
+++ DMCtask2/src/mk/bvj/dmc/model/Session.java
@@ -7,50 +7,53 @@ import java.util.List;
  */
 public class Session {
 
-  private int sessionId;
-  private String order;
-  private List<Transaction> transactions;
-  
-  /**
-   * @return the sessionId
-   */
-  public int getSessionId() {
-    return sessionId;
-  }
-  
-  /**
-   * @param sessionId the sessionId to set
-   */
-  public void setSessionId(int sessionId) {
-    this.sessionId = sessionId;
-  }
-  
-  /**
-   * @return the order
-   */
-  public String getOrder() {
-    return order;
-  }
-  
-  /**
-   * @param order the order to set
-   */
-  public void setOrder(String order) {
-    this.order = order;
-  }
-  
-  /**
-   * @return the transactions
-   */
-  public List<Transaction> getTransactions() {
-    return transactions;
-  }
-  
-  /**
-   * @param transactions the transactions to set
-   */
-  public void setTransactions(List<Transaction> transactions) {
-    this.transactions = transactions;
-  }
-  
+	private long sessionId;
+	private String order;// boolean?
+	private List<Transaction> transactions;
+
+	/**
+	 * @return the sessionId
+	 */
+	public long getSessionId() {
+		return sessionId;
+	}
+
+	/**
+	 * @param sessionId
+	 *            the sessionId to set
+	 */
+	public void setSessionId(int sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	/**
+	 * @return the order
+	 */
+	public String getOrder() {
+		return order;
+	}
+
+	/**
+	 * @param order
+	 *            the order to set
+	 */
+	public void setOrder(String order) {
+		this.order = order;
+	}
+
+	/**
+	 * @return the transactions
+	 */
+	public List<Transaction> getTransactions() {
+		return transactions;
+	}
+
+	/**
+	 * @param transactions
+	 *            the transactions to set
+	 */
+	public void setTransactions(List<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+
 }
